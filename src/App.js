ac4dad5427d3f65e804b539869bc9aab623173be
Ange-09 <santiago.ry.angelo@gmail.com>
2025-10-26 15:52:34 +0800

    psuh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
 import Home from "./pages/Home";
 import OwnerPage from "./pages/OwnerPage";
