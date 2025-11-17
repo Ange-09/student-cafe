8fd043840187814ba5f3042600dd54783b1f0832
Ange-09 <santiago.ry.angelo@gmail.com>
2025-11-17 20:51:51 +0800

    OwnerPage Divisions & Installed Recharts

--- src/pages/OwnerPage.jsx
+++ src/pages/OwnerPage.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import ChatWidgetInbox from "../components/ChatWidgetInbox";
+import OwnerPageSection from "../sections/OwnerPageSection";
 
 function OwnerPage() {
   return (
     <div>
       <Header />
       <ChatWidgetInbox />
+      <OwnerPageSection />
     </div>
   );
 }
---
+++ src/sections/OwnerPageSection.jsx
@@ -0,0 +1,17 @@
+import "../styles/ownerpagesection.css";
+
+function OwnerPageSection() {
+  return (
+    <div className="ownerpagesection">
+      <div className="ops-row-one">CloudSpace Study Hub</div>
+      <div className="ops-row-two">
+        <div className="linegraph"></div>
+        <div className="totalcustomers"></div>
+        <div className="ops-reviews"></div>
+      </div>
+      <div className="ops-row-three"></div>
+    </div>
+  );
+}
+
+export default OwnerPageSection;
